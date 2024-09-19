add658c64cd852bcf1462ac647445eb8a504fdb3
MxAziz <hereabdulaziz@gmail.com>
2024-09-19 12:06:51 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber)
+})
