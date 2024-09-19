88e31ef46258262e95c96b4553e26a251695c7dd
MxAziz <hereabdulaziz@gmail.com>
2024-09-19 12:10:40 +0600

    added simple login handler and preventDefault

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
+    console.log('login button clicked');
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    console.log(phoneNumber);
 })
