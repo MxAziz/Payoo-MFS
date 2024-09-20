e2bef16848dc960537ba4cda3e428e2b29185b86
MxAziz <hereabdulaziz@gmail.com>
2024-09-20 08:48:34 +0600

    Add money to the balance after pin verification

--- js/home.js
+++ js/home.js
@@ -2,7 +2,21 @@
 document.getElementById('btn-add-money').addEventListener('click', function(event){
     event.preventDefault();
 
-    const addMoneyInput = document.getElementById('input-add-money').value ;
+    const addMoneyInput = Number(document.getElementById('input-add-money').value) ;
     const pinInput = document.getElementById('input-pin').value;
     console.log(addMoneyInput, pinInput);
+
+    if(pinInput === '1234'){
+        console.log('adding money');
+        const balance = Number(document.getElementById('account-balance').innerText);
+        let newBalance = balance + addMoneyInput;
+        console.log(newBalance)
+
+        // be careful the next line 
+        document.getElementById('account-balance').innerText = newBalance;
+        // ------------------------------->
+    }
+    else{
+        alert('Failed to add money! Please try again');
+    }
 })
