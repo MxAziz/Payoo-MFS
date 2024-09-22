ce3ff436891dfd3135cdbba87867731abe73dece
MxAziz <hereabdulaziz@gmail.com>
2024-09-22 10:59:18 +0600

    fix home page bug

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById('btn-login').addEventListener('click', function(event){
     const pinNumber = document.getElementById('pin-number').value;
 
     if(phoneNumber === '5' && pinNumber === '1234'){
-        window.location.href = '/home.html';
+        window.location.href = './home.html';
     }
     else{
         alert('Invalid Input');
